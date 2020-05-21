1eaaaa8d493d9c9eabb1237f69f7c3a74f7ca562
Emilie <emilielayani@gmail.com>
2020-05-21 17:37:20 +0300

    Add links to nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
-  return (
-    <nav className="Nav">
-      <h3>Logo</h3>
-      <ul className='nav-links'>
-          <li>About</li>
-          <li>Shop</li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="Nav">
+            <h3>Logo</h3>
+            <ul className='nav-links'>
+                <Link to="/about">
+                    <li>About</li>
+                </Link>
+                <Link to='/shop'>
+                    <li>Shop</li>
+                </Link>
+
+            </ul>
+        </nav>
+    );
 }
 
 export default Nav;
