df6c2d806597b52aa43b0a89397d0769873b7e60
Emilie <emilielayani@gmail.com>
2020-05-21 17:14:17 +0300

    first push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Nav from './Nav';
+import About from './About';
+import Shop from './Shop';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App Page</h1>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
